da189340b15fa474752b77ccbcdffcf8c389d97d
memray <memray0@gmail.com>
2016-01-22 23:56:54 -0500

    migrate to heliordor

--- crawler/pdf_crawl.py
+++ crawler/pdf_crawl.py
@@ -23,20 +23,26 @@ def download(id):
 
 
 
-pdf_dir = "E:/acm_dl/pdf/chi/"
+pdf_dir = ""
+conference_name = "chi"
 
 if __name__ == "__main__":
+    project_root_dir = os.path.dirname(__file__)
+    project_root_dir = project_root_dir[:project_root_dir.rfind('/')]
+
+    pdf_dir = '/home/memray/Data/acm/pdf/'+conference_name+'/'
     if not os.path.exists(pdf_dir):
         os.makedirs(pdf_dir)
     # db=Connection(host='127.0.0.1',database='acm',user='root',password='123456')
     # for row in db.query('select * from acm_article limit 0,100'):
-    id_file = open('data/paperurls/cikm.txt','r')
+    id_file = open(os.path.join(project_root_dir,'data/paperurls/'+conference_name+'.txt'),'r')
+    fail_file = open(os.path.join(project_root_dir,'data/paperurls/fail.txt'),'w')
     count = 0
 
     while True:
         count+=1
         id = id_file.readline().strip()
-        if(id is None):
+        if((id is None) or (len(id)==0)):
             break
 
         # check whether this pdf has been downloaded
@@ -52,7 +58,12 @@ if __name__ == "__main__":
             delay=0
         print('sleep for '+str(delay)+'s')
         sleep(delay)
-        download(id)
+
+        try:
+            download(id)
+        except:
+            fail_file.write(id+'\n')
+
         if(count % 20 == 0):
             delay = random.gauss(120, 4)
             if delay <0:
